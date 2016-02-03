f3e208fcce744a2c914c172a116775cdc0b9db7d
Victor Borja <vborja@apache.org>
2016-02-02 21:09:00 -0600

    init httpoison

--- mix.exs
+++ mix.exs
@@ -16,7 +16,7 @@ defmodule Elaxtic.Mixfile do
   #
   # Type "mix help compile.app" for more information
   def application do
-    [applications: [:logger]]
+    [applications: [:logger, :httpoison]]
   end
 
 
